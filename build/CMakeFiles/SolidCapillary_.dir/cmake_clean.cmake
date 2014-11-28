4bcf2503da4011f922dd24a47de4d6184a477939
Andrea <andrea.benaglia@cern.ch>
2014-11-28 16:15:32 +0100

    remove unnecessary files

--- build/CMakeFiles/SolidCapillary_.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/SolidCapillary_"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/SolidCapillary_.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
